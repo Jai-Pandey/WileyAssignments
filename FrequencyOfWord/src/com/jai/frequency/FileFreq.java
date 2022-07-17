0bf2515d3fd0ef56e2ff4661fad72fd6d83dde14
Jai-Pandey <pandeyjai00001@gmail.com>
2022-07-17 23:57:30 +0530

    second commit

--- FrequencyOfWord/src/com/jai/frequency/FileFreq.java
+++ FrequencyOfWord/src/com/jai/frequency/FileFreq.java
@@ -1,52 +1,44 @@
 package com.jai.frequency;
-import java.io.FileInputStream;
+
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 
 public class FileFreq {
-	BinarySearchTree<String> bst = new BinarySearchTree<>();
-
-	public FileFreq(BinarySearchTree<String> bst) {
-		super();
-		this.bst = bst;
-	}
-
-	public void reader(String fileName) {
-		try (FileInputStream fileInputStream = new FileInputStream(fileName);
-				ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream)) {
-
-			while (true)
-				bst.insert((String) objectInputStream.readObject());
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-
-		} catch (ClassNotFoundException e) {
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		BinarySearchTree f = new BinarySearchTree();
+		try(FileWriter fileWriter=new FileWriter("FrequencyCount");){
+			
+			String str="If you're looking for random paragraphs, you've come to the right place. When a random word or a random sentence isn't quite enough, the next logical step is to find a random paragraph. We created the Random Paragraph Generator with you in mind. The process is quite simple. Choose the number of random paragraphs you'd like to see and click the button. Your chosen number of paragraphs will instantly appear.\r\n"
+					+ "While it may not be obvious to everyone, there are a number of reasons creating random paragraphs can be useful. A few examples of how some people use this generator are listed in the following paragraphs.\r\n"
+					+ "Generating random paragraphs can be an excellent way for writers to get their creative flow going at the beginning of the day. The writer has no idea what topic the random paragraph will be about when it appears. This forces the writer to use creativity to complete one of three common writing challenges. The writer can use the paragraph as the first one of a short story and build upon it. A second option is to use the random paragraph somewhere in a short story they create. The third option is to have the random paragraph be the ending paragraph in a short story. No matter which of these challenges is undertaken, the writer is forced to use creativity to incorporate the paragraph into their writing.\r\n"
+					+ "";
+			fileWriter.write(str);
+			System.out.println("File Created");
+		} 
+		catch (IOException e) {
 			e.printStackTrace();
 		}
-	}
-
-	public void writer(String fileName) {
-		try (FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-				ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
-			objectOutputStream.writeObject("Hola");
-			objectOutputStream.writeObject("Jai");
-			objectOutputStream.writeObject("Prakash");
-			objectOutputStream.writeObject("Narayan");
-			objectOutputStream.writeObject("Panedey");
-			objectOutputStream.writeObject("Aka");
-			objectOutputStream.writeObject("Anant Shahrag");
-			objectOutputStream.writeObject("which");
-			objectOutputStream.writeObject("means");
-			objectOutputStream.writeObject("Endless");
-			objectOutputStream.writeObject("WordSmith");
-			objectOutputStream.writeObject("Thank You");
-
+		
+		try {
+			BufferedReader b = new BufferedReader(new FileReader("FrequencyCount"));
+			String word = null;
+			String wordArr[] =null;
+			while((word = b.readLine())!=null) {
+				wordArr=word.split(("[, . ' / * @ # $ % ^ & < > ? : ; | \\n \\r]+"));
+				 for(String e:wordArr) {
+					 f.insert(e);
+				 }
+			}
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		f.inorderTraversal();		
+
 	}
+
 }
